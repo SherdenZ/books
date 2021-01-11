7870efd0360f2f6563d8862b7c52f236f5685611
SHerdenZ <zuhio76@gmail.com>
2021-01-11 19:38:11 +0400

    test ajax

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,6 +4,10 @@ let load = document.getElementById('load-from-file');
 
 let way = document.getElementById('way-to-add');
 
+let loadBookDescriptrion = document.getElementById('load-book-description');
+
+let loadBookName = document.getElementById('load-book-name');
+
 way.onclick = function(event) {
     let target = event.target;
 
@@ -16,12 +20,31 @@ way.onclick = function(event) {
         show(load);
         hide(self);
     }
-}
+};
 
 function hide(element) {
     element.className = 'hide';
-}
+};
 
 function show (element) {
     element.className = 'show';
+};
+
+let requestUrl = 'https://apiinterns.osora.ru/';
+
+let body = {
+    login: loadBookName.value,
+    file: loadBookDescriptrion.value
 }
+
+function sendRequest (method, url, body = null) {
+    return fetch(url, {
+        method: 'POST',
+        body: body
+    }).then( response => {
+        return response.text();
+    })
+}
+
+/* sendRequest('POST', requestUrl, body)
+    .then(data => console.log(data)); */
