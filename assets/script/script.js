282af64d7ce63c24f7853c3a6be0f6c49451f1e9
SHerdenZ <zuhio76@gmail.com>
2021-01-13 10:13:18 +0400

    feat: save written in localStorage

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,15 +8,23 @@ let loadBookDescriptrion = document.getElementById('load-book-description');
 
 let loadBookName = document.getElementById('load-book-name');
 
+let writeBookName = document.getElementById('write-book-name');
+
+let writeBookDescription = document.getElementById('write-book-description');
+
 let writeAndSave = document.getElementById('write-and-save');
 writeAndSave.addEventListener('click', saveWritten);
 let loadAndSave = document.getElementById('load-and-save');
 loadAndSave.addEventListener('click', saveLoaded);
 
+let resultBlock = document.querySelector('.result');
+
 const requestUrl = 'https://apiinterns.osora.ru/';
 
 way.onclick = function(event) {
     let target = event.target;
+
+    resultBlock.innerHTML = '';
 
     if (target.id == '') return;
 
@@ -38,14 +46,27 @@ function show (element) {
 };
 
 function saveWritten() {
-    alert('written');
+    let book = {name: writeBookName.value, description: writeBookDescription.value};
+    localStorage.setItem('book' + (localStorage.length - 2), book);
+    resultBlock.innerHTML = 'Книга успешно сохранена';
+    event.preventDefault();
+    hide(self);
+    clear(self);
 }
 
 function saveLoaded() {
+    resultBlock.innerHTML = '';
     let login = loadBookName.value;
+    let file = loadBookDescriptrion.files[0];
+
+    if (file.type !== 'text/plain') {
+        resultBlock.innerHTML = 'Загружать только .txt файлы';
+        event.preventDefault();
+        return
+    }
 
     let formData = new FormData();
-    formData.append('file', loadBookDescriptrion.files[0]); /* в 'для распознавания сервером' */
+    formData.append('file', file); /* в ''для распознавания сервером */
     formData.append('login', login);
    
     /* let file = new Blob([formData], {type: 'application/binary'});  --- отправлять именно FormData, а не blob
@@ -56,7 +77,13 @@ function saveLoaded() {
     }; */
 
     sendRequest(formData);
+
+
+
     event.preventDefault(); /* нужна чтоб страница не перезагружалась */
+    resultBlock.innerHTML = 'Книга успешно сохранена.';
+    hide(load);
+    clear(load);
 }
 
 
@@ -70,6 +97,12 @@ function sendRequest (body) {
     })
     /* .then(response => response.json()) */
     .then( response => { 
-        console.log('Пошло');
-        console.log(response )})
+        answer = response.json();
+        console.log(answer);
+    })
+}
+
+function clear(element) {
+    element.children[0].value = '';
+    element.children[1].value = '';
 }
